98fa94478adbedc5ce571a5fa0ac2f935337b9ab
Carlos Veny <carlosveny@yahoo.es>
2023-08-31 09:21:39 +0200

    fix: remove repoName, make everything more dynamic

--- src/showResults.js
+++ src/showResults.js
@@ -5,9 +5,12 @@ const file = 'results.json'
 fs.readFile(file, 'utf8', (err, data) => {
   if (err) throw err
 
-  const dataObject = JSON.parse(data)
+  const [first, ...dataObject] = JSON.parse(data)
   console.log('\x1b[36m%s\x1b[0m', 'RESULTS:')
-  const results = dataObject.map(test => ({
+  const results = [
+    ...dataObject,
+    first
+  ].map(test => ({
     command: test.name,
     'time (seconds)': (test.endTime - test.startTime) / 1000,
     'time (minutes)': parseFloat(((test.endTime - test.startTime) / 60000).toFixed(2))
--- src/startApplication.js
+++ src/startApplication.js
@@ -1,26 +1,20 @@
-const fs = require('fs')
 const process = require('child_process')
 const treeKill = require('tree-kill')
 
-const file = 'config/config.json'
-fs.readFile(file, 'utf8', (_, data) => {
-  const dataObject = JSON.parse(data)
-  const cmd = process.spawn(`cd ${dataObject.repoName} && npm run dev:ssr`, { shell: true })
+const cmd = process.spawn('cd project-to-test && npm run dev:ssr', { shell: true })
 
-  cmd.stdout.on('data', (output) => {
-    if (output.toString().includes('localhost:4200')) {
-      cmd.stdin.pause()
-      treeKill(cmd.pid, 'SIGTERM', function (err) {
-        if (err) {
-          console.error('Error killing process tree:', err)
-        } else {
-          console.log('\x1b[0m', 'Process tree terminated.')
-        }
-      })
-    }
-    console.log(output.toString())
-  })
-
-  cmd.stderr.on('data', (err) => console.log(err.toString()))
-
+cmd.stdout.on('data', (output) => {
+  if (output.toString().includes('localhost:4200')) {
+    cmd.stdin.pause()
+    treeKill(cmd.pid, 'SIGTERM', function (err) {
+      if (err) {
+        console.error('Error killing process tree:', err)
+      } else {
+        console.log('\x1b[0m', 'Process tree terminated.')
+      }
+    })
+  }
+  console.log(output.toString())
 })
+
+cmd.stderr.on('data', (err) => console.log(err.toString()))
